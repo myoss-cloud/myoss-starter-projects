5de6b5466b6b4ac234c6c4e5ecb63e5768643815
Jerry.Chen <jerry.myoss@gmail.com>
2018-04-13 13:39:56 +0800

    feature: SpringWebMvc基类配置和客户端web请求审计日志过滤器

---
+++ src/main/java/io/myoss/phoenix/core/spring/config/FastJsonAutoConfiguration.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2018-2018 https://github.com/myoss
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package io.myoss.phoenix.core.spring.config;
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.google.common.collect.Lists;
+
+import io.myoss.phoenix.core.constants.PhoenixConstants;
+
+/**
+ * Fast Json的全局自动配置
+ *
+ * @author Jerry.Chen 2018年4月12日 下午5:27:16
+ */
+@ConditionalOnClass({ FastJsonConfig.class })
+@ConditionalOnWebApplication
+@Configuration
+public class FastJsonAutoConfiguration {
+    /**
+     * Fast Json的全局配置
+     */
+    public static FastJsonConfig fastJsonConfig() {
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(
+        // 关闭循环引用，输出的json字符串中有："$ref"
+                SerializerFeature.DisableCircularReferenceDetect,
+                // 将 null 字段输出字段名
+                SerializerFeature.WriteMapNullValue);
+        fastJsonConfig.setCharset(PhoenixConstants.DEFAULT_CHARSET);
+        return fastJsonConfig;
+    }
+
+    /**
+     * 使用Fast Json输出json
+     * <p>
+     * 如果直接注入，此转换器会添加在“转换器集合”的最前面，在输出/输入String类型的数据的时候，会将<code>"</code>添加转义符
+     * <p>
+     * 正因为添加了转义符，导致“网关”将String类型的数据发送给“业务系统”、回写给“客户端”，出现了本不应该出现的转义符
+     * <p>
+     * 解决办法：不直接注入，使用 {@link AbstractWebMvcConfigurer#extendMessageConverters} 添加
+     */
+    public static FastJsonHttpMessageConverter fastJsonHttpMessageConverter(FastJsonConfig fastJsonConfig) {
+        FastJsonHttpMessageConverter jsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+        jsonHttpMessageConverter.setDefaultCharset(PhoenixConstants.DEFAULT_CHARSET);
+        List<MediaType> mediaTypes = Lists.newArrayList(MediaType.APPLICATION_JSON, MediaType.APPLICATION_JSON_UTF8);
+        jsonHttpMessageConverter.setSupportedMediaTypes(mediaTypes);
+        jsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
+        return jsonHttpMessageConverter;
+    }
+
+    /**
+     * Fast Json的全局配置，使用 spring 管理，方便项目中替换或者获取此对象
+     */
+    @ConditionalOnMissingBean(name = "defaultFastJsonConfig")
+    @Bean(name = "defaultFastJsonConfig")
+    public FastJsonConfig defaultFastJsonConfig() {
+        return fastJsonConfig();
+    }
+}
---
+++ src/main/java/io/myoss/phoenix/core/spring/web/servlet/support/EmptyBodyCheckingHttpInputMessage.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2018-2018 https://github.com/myoss
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package io.myoss.phoenix.core.spring.web.servlet.support;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PushbackInputStream;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpRequest;
+import org.springframework.util.StreamUtils;
+
+/**
+ * 检查HTTP input message 的 {@linkplain #getBody() body}，如果body中没有值，则返回{@code null}
+ *
+ * @author Jerry.Chen 2018年4月11日 下午3:00:24
+ */
+public class EmptyBodyCheckingHttpInputMessage implements HttpInputMessage {
+    private final HttpHeaders headers;
+    private final InputStream body;
+    private final HttpMethod  method;
+
+    public EmptyBodyCheckingHttpInputMessage(HttpInputMessage inputMessage) throws IOException {
+        this.headers = inputMessage.getHeaders();
+        InputStream inputStream = inputMessage.getBody();
+        if (inputStream.markSupported()) {
+            inputStream.mark(1);
+            this.body = (inputStream.read() != -1 ? inputStream : null);
+            inputStream.reset();
+        } else {
+            PushbackInputStream pushbackInputStream = new PushbackInputStream(inputStream);
+            int b = pushbackInputStream.read();
+            if (b == -1) {
+                this.body = null;
+            } else {
+                this.body = pushbackInputStream;
+                pushbackInputStream.unread(b);
+            }
+        }
+        this.method = ((HttpRequest) inputMessage).getMethod();
+    }
+
+    @Override
+    public HttpHeaders getHeaders() {
+        return this.headers;
+    }
+
+    @Override
+    public InputStream getBody() {
+        return (this.body != null ? this.body : StreamUtils.emptyInput());
+    }
+
+    public boolean hasBody() {
+        return (this.body != null);
+    }
+
+    public HttpMethod getMethod() {
+        return this.method;
+    }
+}

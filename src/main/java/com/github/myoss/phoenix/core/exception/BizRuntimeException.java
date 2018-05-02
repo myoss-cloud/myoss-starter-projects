7e239931b9c2309267e56ae731da29dac07100ed
Jerry.Chen <jerry.myoss@gmail.com>
2018-05-02 09:51:44 +0800

    feature: 增加业务运行时异常

---
+++ src/main/java/com/github/myoss/phoenix/core/exception/BizRuntimeException.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018-2018 https://github.com/myoss
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.myoss.phoenix.core.exception;
+
+import lombok.Getter;
+
+/**
+ * 业务运行时异常
+ *
+ * @author Jerry.Chen 2018年5月2日 上午1:13:57
+ */
+public class BizRuntimeException extends RuntimeException {
+    private static final long serialVersionUID = 7634892282599474024L;
+    @Getter
+    private String            messageCode;
+
+    public BizRuntimeException() {
+        super();
+    }
+
+    public BizRuntimeException(String message) {
+        super(message);
+    }
+
+    public BizRuntimeException(Throwable cause) {
+        super(cause);
+    }
+
+    public BizRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BizRuntimeException(String messageCode, String message, Throwable cause) {
+        super(message, cause);
+        this.messageCode = messageCode;
+    }
+}

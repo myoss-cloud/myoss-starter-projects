a5f60105acc6c8bde5a5951e0ecabfdbefecfa52
Jerry.Chen <jerry.myoss@gmail.com>
2018-07-15 13:11:24 +0800

    feature: 将字符串的第一位转为小/大写

--- src/main/java/com/github/myoss/phoenix/core/lang/base/StringUtil.java
+++ src/main/java/com/github/myoss/phoenix/core/lang/base/StringUtil.java
@@ -81,4 +81,36 @@ public class StringUtil {
     public static String toPascalCase(String source) {
         return toCamelCase(source, true);
     }
+
+    /**
+     * 将字符串的第一位转为小写
+     *
+     * @param source 需要转换的字符串
+     * @return 转换后的字符串
+     */
+    public static String toLowerCaseFirstOne(String source) {
+        if (Character.isLowerCase(source.charAt(0))) {
+            return source;
+        } else {
+            char[] chars = source.toCharArray();
+            chars[0] = Character.toLowerCase(chars[0]);
+            return new String(chars);
+        }
+    }
+
+    /**
+     * 将字符串的第一位转为大写
+     *
+     * @param source 需要转换的字符串
+     * @return 转换后的字符串
+     */
+    public static String toUpperCaseFirstOne(String source) {
+        if (Character.isUpperCase(source.charAt(0))) {
+            return source;
+        } else {
+            char[] chars = source.toCharArray();
+            chars[0] = Character.toUpperCase(chars[0]);
+            return new String(chars);
+        }
+    }
 }
